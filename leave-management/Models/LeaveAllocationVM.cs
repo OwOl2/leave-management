0623111f6c36fdd48436d4aa2e1e28e75c26f2f1
Matias Dotta <matias.dotta@globant.com>
2021-05-25 01:05:50 -0300

    Add project files.

---
+++ leave-management/Models/LeaveAllocationVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Models
+{
+    public class LeaveAllocationVM
+    {
+        public int Id { get; set; }
+        [Required]
+        public int NumberOfDays { get; set; }
+        public DateTime DateCreated { get; set; }
+        public EmployeeVM Employee { get; set; }
+        public string EmployeeId { get; set; }
+        public DetailsLeaveTypeVM LeaveType { get; set; }
+        public int LeaveTypeId { get; set; }
+        IEnumerable<EmployeeVM> Employees { get; set; }
+        IEnumerable<DetailsLeaveTypeVM> LeaveTypes { get; set; }
+    }
+}
